9eb98eff7a6adc473feb9ec61480b83b4390a731
Jack <jiahaoma2000@gmail.com>
2023-03-11 16:11:45 -0500

    test

--- djangoproject/views.py
+++ djangoproject/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import *
 
 def index(request):
     return render(request, 'index.html')
+
+class ReactView(APIView):
+    def get(self, request):
+        output =  [{"name":output.name, "id": output.id, 
+                    "zip_code":output.zip_code, "email":output.email,
+                    "phone":output.phone, "age":output.age, "hobbies":output.hobbies,
+                    "weather":output.weather, "budget":output.budget, "env_pref":output.env_pref,
+                    "final_init":output.final_init}
+                    for output in React.objects.all()]
+        return Response(output)
+
+
+    def post(self, request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+
+
+
+
+
